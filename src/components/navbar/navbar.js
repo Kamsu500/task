ce6f5401518cb65f09d136db1d7fc2bfc48a0657
Kamsu10 <kamsudylane@gmail.com>
2022-04-02 21:14:08 +0200

    last

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,31 +1,44 @@
 import React from "react";
 import './navbar.css'
+import {Link,BrowserRouter as Router} from "react-router-dom";
 // function to display a navbar
 function Navbar(){
 
-    return <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <link class="navbar-brand" href="#">TASK APP</link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <link>Home</link>
-          </li>
-          <li class="nav-item">
-            <link class="nav-link" href="#">Service</link>
-          </li>
-        </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
-     
+    return <Router>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+            <Link class="navbar-brand" to=''>TASK APP</Link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <Link class="nav-link active" aria-current="page" to=''>Home</Link>
+                </li>
+                <li class="nav-item">
+                    <Link class="nav-link" to=''>Service</Link>
+                </li>
+                <li class="nav-item dropdown">
+                    <Link class="nav-link dropdown-toggle" to='' id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    list
+                    </Link>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><Link class="dropdown-item" to=''>Action</Link></li>
+                    <li><Link class="dropdown-item" to=''>Another action</Link></li>
+                    <li><hr class="dropdown-divider"/></li>
+                    <li><Link class="dropdown-item" to=''>Something else here</Link></li>
+                    </ul>
+                </li>
+                </ul>
+                <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+            </div>
+        </nav>
+    </Router>         
 }
 
 export default Navbar
