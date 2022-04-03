88636dbed5b50304d4083ab1a9a45596906e2d56
Kamsu10 <kamsudylane@gmail.com>
2022-04-03 22:10:13 +0200

    last

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,33 +3,45 @@ import './home.css'
 
 export default function Home() {
     return(
-        <div class="container mt-3">
+        <div class="container-fluid mt-3">
         <h2>Affichage des tâches</h2>
-        <table class="table">
-          <thead class="table-success">
+        <div class="row justify-content-between">
+        <div class="col-6">
+          <button type="button" class="btn btn-primary">Add one task</button>
+        </div>
+        <div class="col-6">
+          <h3 class='text-info'>0 enregistrements</h3>
+        </div>
+        </div>
+        <table class="table table-bordered table-light mt-3">
+        <thead>
             <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Utilisateur</th>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Utilisateur</th>
             </tr>
-          </thead>
-          <tbody>
+        </thead>
+        <tbody>
             <tr>
-              <td>John</td>
-              <td>Doe</td>
-              <td>john@example.com</td>
-            </tr>
-            <tr>
-              <td>Mary</td>
-              <td>Moe</td>
-              <td>mary@example.com</td>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
             </tr>
             <tr>
-              <td>July</td>
-              <td>Dooley</td>
-              <td>july@example.com</td>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
             </tr>
-          </tbody>
+            <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
         </table>
       </div>
     )
